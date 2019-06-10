49edc5e15d5af2885888cb2488acebad6dd8f103
Vania Kucher <dev.kucher@gmail.com>
2019-06-10 14:10:35 +0300

    Update 2017_11_18_172759_create_roles_table.php (#1)

--- src/migrations/2017_11_18_172759_create_roles_table.php
+++ src/migrations/2017_11_18_172759_create_roles_table.php
@@ -20,7 +20,11 @@ class CreateRolesTable extends Migration
     public function up()
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->increments('id');
+            if (((float) app()->version()) >= 5.8) {
+                $table->bigIncrements('id');
+            } else {
+                $table->increments('id');
+            }
             $table->string('name');
             $table->timestamps();
         });
